ea3112cebc74abf12c8b521f2091e8a5e04f1a24
ajdin <ajdika@live.com>
2016-11-28 10:57:18 +0000

    l33

---
+++ l33/l33-emitter.js
@@ -0,0 +1,9 @@
+function Emitter() {
+    this.events = {};
+}
+
+// "Emitter.prototype." = prototype of any object created with 
+// the above constructor function
+Emitter.prototype.on = function() {
+    
+}
